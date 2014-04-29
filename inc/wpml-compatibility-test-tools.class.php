980393ec1f491b0305a96e72732d6602570ce6ea
Pawel Wawrzyniak <pawel.w@icanlocalize.com>
2014-04-29 13:54:41 +0000

    Refactoring - changed menu folder structure
    git-svn-id: https://www.onthegosystems.com/misc_svn/wpml/theme-tester/trunk@21863 d15b4b63-6738-4940-a0f7-2beb50815f4f

--- inc/wpml-compatibility-test-tools.class.php
+++ inc/wpml-compatibility-test-tools.class.php
@@ -216,7 +216,7 @@ class WPML_Compatibility_Test_Tools extends WPML_Compatibility_Test_Tools_Base {
 	}
 
 	public function administration_page() {
-		include( WPML_CTT_PATH . '/menus/menu-settings.php' );
+		include( WPML_CTT_PATH . '/menus/settings/settings.php' );
 	}
 
 	public function add_scripts(){
