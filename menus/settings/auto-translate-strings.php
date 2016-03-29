a7bacee9d131688bda5e15afdc609b3f7df0d01e
Vuk Vukovic <vuk.v@icanlocalize.com>
2016-03-29 13:12:42 +0200

    Context checkboxes are checked now by default.

--- menus/settings/auto-translate-strings.php
+++ menus/settings/auto-translate-strings.php
@@ -28,7 +28,7 @@
                                 <?php foreach ( $stt_context as $v ) : ?>
                                     <li>
                                         <input type="checkbox" id="strings_auto_translate_context" name="strings_auto_translate_context[]" value="<?php echo htmlspecialchars( $v->context ); ?>"
-                                            <?php checked( is_array( $string_auto_translate_context ), in_array( htmlspecialchars( $v->context ), $string_auto_translate_context) ); ?>>
+                                            <?php echo ! empty( $string_auto_translate_context ) ? checked( is_array( $string_auto_translate_context ), in_array( htmlspecialchars( $v->context ), $string_auto_translate_context ), false ) : 'checked'; ?>>
                                         <?php echo $v->context . ' ('. $v->c .')'; ?>
                                     </li>
                                 <?php endforeach; ?>
