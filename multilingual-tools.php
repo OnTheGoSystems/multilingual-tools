421da401702c8ce5f31378be6f8df4f6c6357c2b
Vuk Vukovic <vuk@vuk.vu>
2017-02-05 23:35:10 +0100

    Generate shortcodes feature
    * Add option for generating shortcodes.
    * Refactor XML generator code.

--- multilingual-tools.php
+++ multilingual-tools.php
@@ -5,10 +5,10 @@ Plugin URI: https://wpml.org/download/multilingual-tools/
 Description: Set of tools to test themes and plugins multilingual compatibility.
 Author: OnTheGoSystems
 Author URI: http://wpml.org
-Version: 1.2.3
+Version: 1.3.0
 */
 
-define( 'WPML_CTT_VERSION'	 , '1.2.3' );
+define( 'WPML_CTT_VERSION'	 , '1.3.0' );
 define( 'WPML_CTT_PATH'		 , dirname( __FILE__ ) );
 define( 'WPML_CTT_ABS_PATH'	 , plugin_dir_path( __FILE__ ) );
 define( 'WPML_CTT_FOLDER'	 , basename( WPML_CTT_PATH ) );
