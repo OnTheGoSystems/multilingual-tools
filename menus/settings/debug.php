f271e052c54ad9ea830ddc8feffe6bc48a990dd9
David Garcia Watkins <david.g@onthegosystems.com>
2017-10-30 17:20:04 +0100

    [FIXUP] Avoid warnings when there are empty sections.

--- menus/settings/debug.php
+++ menus/settings/debug.php
@@ -10,13 +10,22 @@
 	global $wpml_config_debug;
 
 	// TODO: Administration texts and language switcher settings.
-	$data = array(
-		'Custom posts'                 => $wpml_config_debug['wpml-config']['custom-types']['custom-type'],
-		'Custom taxonomies'            => $wpml_config_debug['wpml-config']['taxonomies']['taxonomy'],
-		'Custom fields translation'    => $wpml_config_debug['wpml-config']['custom-fields']['custom-field'],
-		'Custom Term Meta Translation' => $wpml_config_debug['wpml-config']['custom-term-fields']['custom-term-field'],
-		'Shortcodes'                   => $wpml_config_debug['wpml-config']['shortcodes']['shortcode'],
-	);
+	$data = array();
+	if ( ! empty( $wpml_config_debug['wpml-config']['custom-types']['custom-type'] ) ) {
+		$data['Custom posts'] = $wpml_config_debug['wpml-config']['custom-types']['custom-type'];
+	}
+	if ( ! empty( $wpml_config_debug['wpml-config']['taxonomies']['taxonomy'] ) ) {
+		$data['Custom taxonomies'] = $wpml_config_debug['wpml-config']['taxonomies']['taxonomy'];
+	}
+	if ( ! empty( $wpml_config_debug['wpml-config']['custom-fields']['custom-field'] ) ) {
+		$data['Custom fields translation'] = $wpml_config_debug['wpml-config']['custom-fields']['custom-field'];
+	}
+	if ( ! empty( $wpml_config_debug['wpml-config']['custom-types']['custom-term-fields']['custom-term-field'] ) ) {
+		$data['Custom Term Meta Translation'] = $wpml_config_debug['wpml-config']['custom-term-fields']['custom-term-field'];
+	}
+	if ( ! empty( $wpml_config_debug['wpml-config']['shortcodes']['shortcode'] ) ) {
+		$data['Shortcodes'] = $wpml_config_debug['wpml-config']['shortcodes']['shortcode'];
+	}
 	?>
 	<?php foreach ( $data as $type => $config ) : ?>
 		<?php if ( ! empty( $config ) ) : ?>
