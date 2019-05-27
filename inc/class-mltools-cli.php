bf9ed1be2b60307fd13775f37266417ba4cb6a90
srdjan <srdjan@icanlocalize.com>
2019-05-27 11:03:32 +0200

    Minor fixes

--- inc/class-mltools-cli.php
+++ inc/class-mltools-cli.php
@@ -2,21 +2,28 @@
 
 class MLTools_CLI {
 
-	private $sitepress, $cct;
-
-	function __construct( SitePress $sitepress, WPML_Compatibility_Test_Tools $cct ) {
-		$this->sitepress = $sitepress;
-		$this->cct       = $cct;
-	}
-
-	/*public function __invoke( $args ) {
-		WP_CLI::success( $args[0], $args );
-	}*/
+	private $sitepress, $ctt, $original_settings;
 
 	private $settings = 'a:6:{s:30:"string_auto_translate_template";s:35:"[%language_name%] %original_string%";s:26:"duplicate_strings_template";s:35:"[%language_code%] %original_string%";s:22:"shortcode_enable_debug";b:0;s:28:"shortcode_enable_debug_value";b:0;s:22:"shortcode_ignored_tags";b:0;s:17:"duplicate_strings";a:4:{s:4:"post";a:3:{s:5:"title";s:1:"1";s:7:"content";s:1:"1";s:7:"excerpt";s:1:"1";}s:12:"custom_field";a:1:{s:5:"value";s:1:"1";}s:8:"taxonomy";a:1:{s:3:"all";s:1:"1";}s:13:"taxonomy_slug";a:1:{s:3:"all";s:1:"1";}}}';
 
+	function __construct( SitePress $sitepress, WPML_Compatibility_Test_Tools $ctt ) {
+		$this->sitepress         = $sitepress;
+		$this->ctt               = $ctt;
+		$this->original_settings = get_option( 'wpml_ctt_settings' );
+		update_option( $this->ctt::OPTIONS_NAME, maybe_unserialize( $this->settings ) );
+		// Fix missing tables
+		WPML_Package_Translation_Schema::run_update();
+	}
+
 	/**
 	 * Duplicates posts.
+	 *
+	 * * ---
+	 * default: success
+	 * options:
+	 *   - success
+	 *   - error
+	 * ---
 	 *
 	 * ## EXAMPLES
 	 *
@@ -25,11 +32,6 @@ class MLTools_CLI {
 	 * @when after_wp_load
 	 */
 	public function duplicate() {
-
-		$settings = maybe_unserialize( $this->settings );
-		// Fix missing tables
-		WPML_Package_Translation_Schema::run_update();
-		update_option( 'wpml_ctt_settings', $settings );
 
 		$default_language = $this->sitepress->get_default_language();
 		$languages        = array_keys( $this->sitepress->get_active_languages() );
@@ -64,14 +66,18 @@ class MLTools_CLI {
 				// @todo Translate page-builder strings
 			}
 
-			if ( ! empty( $duplicated_to ) ) {
-				WP_CLI::log( "-- post_ID: {$post_id} [" . implode( ',', $duplicated_to ) . "]" );
-			} else {
-				WP_CLI::error( 'no posts for duplication' );
+			if ( empty( $duplicated_to ) ) {
+				$duplicated_to = array( 'none' );
 			}
+
+			WP_CLI::log( "-- post_ID: {$post_id} [" . implode( ',', $duplicated_to ) . "]" );
 		}
 
-		WP_CLI::success( "duplicated {$count} post(s)" );
+		if ( $count ) {
+			WP_CLI::success( "duplicated {$count} post(s)" );
+		} else {
+			WP_CLI::error( 'no posts for duplication' );
+		}
 	}
 
 	/**
@@ -108,13 +114,17 @@ class MLTools_CLI {
 			case 'string':
 			case 'strings':
 			default:
-				$this->cct->translate_strings_in_context(
+				$this->ctt->translate_strings_in_context(
 					$context,
 					array_keys( $this->sitepress->get_active_languages() ),
 					$settings['string_auto_translate_template']
 				);
 		}
 
-		WP_CLI::success( "strings translated" );
+		WP_CLI::success( 'strings translated' );
+	}
+
+	function __destruct() {
+		update_option( $this->ctt::OPTIONS_NAME, $this->original_settings );
 	}
 }
