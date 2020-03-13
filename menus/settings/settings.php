e0c2f3c043bf25fd2b9bf6f1cfef6a307e14be9c
Vuk Vukovic <vuk@vuk.vu>
2020-03-13 22:18:38 +0100

    Add load_config_run() load to the settings page

--- menus/settings/settings.php
+++ menus/settings/settings.php
@@ -2,6 +2,7 @@
     <div id="icon-wpml" class="icon32"><br /></div>
     <h2><?php _e('Settings', 'wpml-compatibility-test-tools'); ?></h2>
 	<?php
+	WPML_Config::load_config_run();
 
 	//include strings auto translate box only when String Translations plugin is available
 	if ( defined( 'WPML_ST_VERSION' )  ) {
